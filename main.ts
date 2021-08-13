f0c6bcf718df857f001fcc64537a6120352bc3af
rpiumpun <73171407+rpiumpun@users.noreply.github.com>
2021-08-13 12:48:17 +0700

    add obsticals

--- main.ts
+++ main.ts
@@ -5,10 +5,16 @@ input.onButtonPressed(Button.B, function () {
     human.change(LedSpriteProperty.Y, -1)
 })
 let human: game.LedSprite = null
-let rand = randint(0, 4)
 human = game.createSprite(0, 2)
 let gameover = false
-let rock: number[] = []
-basic.forever(function () {
-	
-})
+let rock: game.LedSprite[] = []
+let index = 0
+let rockY: number[] = []
+for (let index2 = 0; index2 < 3; index2++) {
+    rockY.push(randint(0, 4))
+}
+for (let index = 0; index <= 4; index++) {
+    if (index != rockY[index]) {
+        rock.push(game.createSprite(2, rockY[index]))
+    }
+}
